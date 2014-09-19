f775eff38abf2588825c2d6eb1b8083848b19017
Andreas Abel <andreas.abel@ifi.lmu.de>
2014-09-19 10:56:21 +0200

    [ Fomega ] Implemented inductive instance of grammar.

--- src/full/Agda/Compiler/Fomega/Syntax/Inductive.hs
+++ src/full/Agda/Compiler/Fomega/Syntax/Inductive.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+
 -- | Compile Agda to System Fω with data types and constructor.
 
 module Agda.Compiler.Fomega.Syntax.Inductive where
@@ -151,6 +155,9 @@ import Agda.Syntax.Abstract.Name
 import qualified Agda.Syntax.Internal as I
 import Agda.Syntax.Literal
 
+import Agda.Compiler.Fomega.Syntax (KindRep(..), TypeRep(..), ExprRep(..), ArgInfo(..), Arg(..))
+import qualified Agda.Compiler.Fomega.Syntax as F
+
 -- | System F omega kinds.
 data Kind
   = KBase
@@ -162,28 +169,23 @@ data Kind
 data Type
   = TVar {-# UNPACK #-} !TVar TyArgs
     -- ^ Type (constructor) variables (applied to types).
-  | TCon TCon TyArgs
-    -- ^ Type constructor.
+  | TArrow Type Type
+    -- ^ Function type.
+  | TForall Kind (I.Abs Type)
+    -- ^ Polymorphic type.
+  | TCon QName TyArgs
+    -- ^ Data or record type constructor application.
   | TLam (I.Abs Type)
     -- ^ Type abstraction @λX.T@.
-  | Unknown
+  | TUnknown
     -- ^ A type coming from Agda that is not representable in System Fω.
-  | Erased
+  | TErased
     -- ^ Type of erased things (proofs etc.)
 
 type TyArgs = [Type]
 
 -- | Type variables are represented by de Bruijn indices.
 type TVar = Int
-
--- | Type constructors
-data TCon
-  = TArrow
-    -- ^ Function space.
-  | TForall
-    -- ^ Polymorphic type.
-  | TData QName
-    -- ^ User defined data and record types.
 
 -- | System F expressions.
 data Expr
@@ -197,27 +199,77 @@ data Expr
     -- ^ Defined function @f es@.
   | FCon I.ConHead Args
     -- ^ Data constructor @c es@.
-  | Coerce Expr
+  | FCoerce Expr
     -- ^ Type cast (used for expressions that are well-typed in Agda,
     --   but ill-typed in Fω).
 
 -- | Term variables are de Bruijn indices.
 type FVar = Int
 
--- | Classification of arguments in expression application
-data ArgInfo
-  = TypeArg
-    -- ^ Type argument.
-  | TermArg
-    -- ^ Term argument.
-
--- | Decorated arguments.
-data Arg a = Arg
-  { argInfo :: ArgInfo
-    -- ^ Argument decoration.
-  , arg     :: a
-    -- ^ The argument.
-  }
-
 -- | List of arguments.
 type Args = [Arg Expr]
+
+
+-- * Instantiating the abstract interface for kinds.
+
+type KindView = F.KindView' Kind
+
+instance Monad m => KindRep m Kind where
+
+  -- kindView :: Monad m => Kind -> m KindView
+  kindView KBase       = return $ F.KBase
+  kindView (KArr k k') = return $ F.KArr k k'
+
+  kBase = KBase
+  kArr  = KArr
+
+
+-- * Instantiating the abstract interface for types.
+
+type TypeView = F.TypeView' Kind Type
+
+instance Monad m => TypeRep m Type where
+
+  type KindRep_ Type = Kind
+
+  -- typeView :: Monad m => Type -> m TypeView
+  typeView t =
+    case t of
+      TVar i ts   -> return $ F.TVar i ts
+      TArrow t u  -> return $ F.TArrow t u
+      TForall k t -> return $ F.TForall k t
+      TCon d ts   -> return $ F.TCon d ts
+      TLam t      -> return $ F.TLam t
+      TUnknown    -> return $ F.TUnknown
+      TErased     -> return $ F.TErased
+
+  tVar    = TVar
+  tArrow  = TArrow
+  tForall = TForall
+  tCon    = TCon
+  tLam    = TLam
+  tErased = TErased
+
+
+-- * Instantiating the abstract interface for expressions.
+
+type ExprView = F.ExprView' Expr
+
+instance Monad m => ExprRep m Expr where
+
+  -- exprView :: Expr -> m ExprView
+  exprView e =
+    case e of
+      FVar i es -> return $ F.FVar i es
+      FLam ai f -> return $ F.FLam ai f
+      FLit l    -> return $ F.FLit l
+      FDef d es -> return $ F.FDef d es
+      FCon c es -> return $ F.FCon c es
+      FCoerce e -> return $ F.FCoerce e
+
+  var    = FVar
+  lam    = FLam
+  lit    = FLit
+  def    = FDef
+  con    = FCon
+  coerce = FCoerce
