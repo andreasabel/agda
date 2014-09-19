d86f1240b0c8059367ad18c2b8afc8b9ccf08a9d
Andreas Abel <andreas.abel@ifi.lmu.de>
2014-09-19 15:52:44 +0200

    [ Fomega ] Fixed compilation of AgdaInternal.

--- src/full/Agda/Compiler/Fomega/Syntax/AgdaInternal.hs
+++ src/full/Agda/Compiler/Fomega/Syntax/AgdaInternal.hs
@@ -6,7 +6,7 @@
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeSynonymInstances  #-}  -- ghc >= 7.0
 
-#if __GLASGOW_HASKELL__ >= 706
+#if __GLASGOW_HASKELL__ >= 709
 {-# LANGUAGE InstanceSigs          #-} -- too new, ghc >= 7.6
 #endif
 
@@ -39,7 +39,7 @@ type KindView = KindView' Kind
 --   using weak head evaluation on the type checking monad @TCM@.
 instance KindRep TCM Kind where
 
-#if __GLASGOW_HASKELL__ >= 706
+#if __GLASGOW_HASKELL__ >= 709
   kindView :: Kind -> TCM KindView
 #endif
   kindView t = do
@@ -51,7 +51,7 @@ instance KindRep TCM Kind where
       I.ExtLam{}   -> __IMPOSSIBLE__
       I.Lit{}      -> __IMPOSSIBLE__
       I.Con{}      -> __IMPOSSIBLE__
-      I.Level{}    -> __IMPOSSIBLE__
+      I.Level{}    -> return $ KTerm
       -- The following is excluded by ignoreSharing.
       I.Shared{}   -> __IMPOSSIBLE__
       -- We cannot have an irrelevance marker in a type.
@@ -64,13 +64,16 @@ instance KindRep TCM Kind where
       I.Var{}      -> return $ KType
       I.Def{}      -> return $ KType
 
-#if __GLASGOW_HASKELL__ >= 706
+#if __GLASGOW_HASKELL__ >= 709
   kType :: Kind
 #endif
   kType     = Sort $ mkType 0
   -- Note: we do not care about the sort here.
 
-#if __GLASGOW_HASKELL__ >= 706
+  -- | We abuse Level to represent KTerm.
+  kTerm = Level $ Max []
+
+#if __GLASGOW_HASKELL__ >= 709
   kArrow :: Kind -> Kind -> Kind
 #endif
   kArrow k k' = Pi (defaultDom $ El Inf k) (NoAbs "_" $ El Inf k')
@@ -79,7 +82,6 @@ instance KindRep TCM Kind where
 -- * Implementation of types.
 
 type Type     = I.Term
-type TCon     = TCon' Kind
 type TyArgs   = TyArgs' Type
 type TypeView = TypeView' Kind Type
 
@@ -88,7 +90,7 @@ instance TypeRep TCM Type where
   type KindRep_ Type = Kind
 
 
-#if __GLASGOW_HASKELL__ >= 706
+#if __GLASGOW_HASKELL__ >= 709
   -- typeView :: Type -> TCM TypeView
   typeView :: Type -> TCM (TypeView' (KindRep_ Type) Type)
 #endif
@@ -96,9 +98,8 @@ instance TypeRep TCM Type where
     t <- reduce t
     case ignoreSharing t of
       I.Pi dom b   -> case b of
-        NoAbs _ b -> return $ TCon TArrow [unEl $ unDom dom, unEl b]
-        Abs{}     -> return $ TCon (TForall $ unEl $ unDom dom) [I.Lam defaultArgInfo $ unEl <$> b]
-
+        NoAbs _ b  -> return $ TArrow (unEl $ unDom dom) (unEl b)
+        Abs{}      -> return $ TForall (unEl $ unDom dom) (unEl <$> b)
       I.Lam _ t    -> return $ TLam t
       -- The following are not types:
       I.ExtLam{}   -> __IMPOSSIBLE__
@@ -114,29 +115,23 @@ instance TypeRep TCM Type where
       -- Universes are collapsed into base kind *.
       I.Sort{}     -> return $ TErased
       I.Var i es   -> return $ TVar i $ map unArg $ fromMaybe __IMPOSSIBLE__ $ allApplyElims es
-      I.Def d es   -> return $ TCon (TData d) $ map unArg $ fromMaybe __IMPOSSIBLE__ $ allApplyElims es
+      I.Def d es   -> return $ TCon d $ map unArg $ fromMaybe __IMPOSSIBLE__ $ allApplyElims es
 
-#if __GLASGOW_HASKELL__ >= 706
+#if __GLASGOW_HASKELL__ >= 709
   tVar :: TVar -> TyArgs -> Type
 #endif
   tVar i ts = I.Var i $ map (Apply . defaultArg) ts
 
-#if __GLASGOW_HASKELL__ >= 706
-  -- tCon :: TCon -> TyArgs -> Type  -- FAILS
-  -- tCon :: TCon' Kind -> TyArgs -> FAILS
-  tCon :: TCon' (KindRep_ Type) -> TyArgs -> Type -- OK
-#endif
-  tCon (TData  q) ts = I.Def q $ map (Apply . defaultArg) ts
-  tCon (TArrow) [t,t'] = I.Pi (defaultDom $ El Inf t) (NoAbs "_" $ El Inf t')
-  tCon (TForall k) [I.Lam _ t] = I.Pi (defaultDom $ El Inf k) (El Inf <$> t)
-  tCon _ _ = __IMPOSSIBLE__
+  tCon q ts = I.Def q $ map (Apply . defaultArg) ts
+  tArrow t t' = I.Pi (defaultDom $ El Inf t) (NoAbs "_" $ El Inf t')
+  tForall k t = I.Pi (defaultDom $ El Inf k) (El Inf <$> t)
 
-#if __GLASGOW_HASKELL__ >= 706
+#if __GLASGOW_HASKELL__ >= 709
   tLam :: I.Abs Type -> Type
 #endif
   tLam t = I.Lam defaultArgInfo t
 
-#if __GLASGOW_HASKELL__ >= 706
+#if __GLASGOW_HASKELL__ >= 709
   tErased :: Type
 #endif
   tErased = I.Sort $ mkType 0
