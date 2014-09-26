b674f300e7757104c8957a27bece0f3756ae7db5
Andreas Abel <andreas.abel@ifi.lmu.de>
2014-09-26 11:40:47 +0200

    [ Fomega ] Pretty printing of kinds.

---
+++ src/full/Agda/Compiler/Fomega/Syntax/Pretty.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverlappingInstances  #-}
+{-# LANGUAGE UndecidableInstances  #-}
+
+-- | Pretty printer for Fω syntax
+
+module Agda.Compiler.Fomega.Syntax.Pretty where
+
+import Control.Applicative
+
+import Agda.Compiler.Fomega.Syntax
+
+import Agda.Utils.Pretty
+
+-- * Kinds
+
+-- | Note: function space in domain @(k1 -> k2) -> k3@
+--   vs. function space in range @k1 -> k2 -> k3@.
+instance Pretty a => Pretty (KindView' a) where
+  prettyPrec n k =
+    case k of
+      KType        -> text "*"
+      KTerm        -> text "()"
+      KArrow k1 k2 -> mparens inDom $ d1 <+> text "->" <+> d2
+        where
+          d1      = prettyPrec domPrec k1
+          d2      = prettyPrec rngPrec k2
+          domPrec = 1
+          rngPrec = 0
+
+          inDom   = n >= domPrec
+
+-- | Note: function space in domain @(k1 -> k2) -> k3@
+--   vs. function space in range @k1 -> k2 -> k3@.
+instance (Functor m, Applicative m, MonadPretty m a) => MonadPretty m (KindView' a) where
+  prettyPrecM n k =
+    case k of
+      KType        -> return $ text "*"
+      KTerm        -> return $ text "()"
+      KArrow k1 k2 -> arr <$> prettyPrecM domPrec k1 <*> prettyPrecM rngPrec k2
+        where
+          arr d1 d2 = mparens inDom $ d1 <+> text "->" <+> d2
+          inDom   = n >= domPrec
+          domPrec = 1
+          rngPrec = 0
+
+-- Why does GHC-7.8.3 insist on an "Applicative m" constraint here?!
+instance (Applicative m, KindRep m a) => MonadPretty m a where
+  prettyPrecM n a = prettyPrecM n =<< kindView a
