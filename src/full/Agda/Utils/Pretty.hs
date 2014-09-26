14d4a3901574b946f982b317af4abdf246e627e0
Andreas Abel <andreas.abel@ifi.lmu.de>
2014-09-26 18:25:07 +0200

    [ Utils ] Precedence monad for pretty printing.

--- src/full/Agda/Utils/Pretty.hs
+++ src/full/Agda/Utils/Pretty.hs
@@ -1,6 +1,11 @@
+{-# OPTIONS_GHC -fwarn-missing-signatures #-}
+
 {-# LANGUAGE CPP                  #-}
-{-# LANGUAGE FlexibleInstances    #-}
-{-# LANGUAGE MultiParamTypeClasses  #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE FunctionalDependencies     #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 
 {-| Pretty printing functions.
@@ -9,6 +14,10 @@ module Agda.Utils.Pretty
     ( module Agda.Utils.Pretty
     , module Text.PrettyPrint
     ) where
+
+import Control.Applicative
+import Control.Monad.Reader
+import Control.Monad.Trans
 
 import Data.Function
 import Data.Int ( Int32 )
@@ -21,6 +30,9 @@ import Agda.Utils.Null
 instance Null Doc where
   empty = mempty
   null  = (== mempty)
+
+import Agda.Utils.Functor
+import Agda.Utils.Monad
 
 -- * Pretty class
 
@@ -61,6 +73,8 @@ instance Pretty String where
 
 -- * Monadic pretty-printing
 
+-- ** Top precedence
+
 -- | Abstract type of precedences.
 
 class TopPrecedence p where
@@ -73,17 +87,66 @@ instance TopPrecedence () where
 instance TopPrecedence Int where
   topPrecedence = 0
 
+-- ** Precedence reader
+
+-- | Precedence context monad.  A copy of 'MonadReader'.
+
+class MonadPrec p m | m -> p where
+  askPrec   :: m p
+  localPrec :: (p -> p) -> m a -> m a
+
+-- | Continue in top context.
+inTopPrec :: (TopPrecedence p, MonadPrec p m) => m a -> m a
+inTopPrec = localPrec $ const topPrecedence
+
+-- -- | No precedence information.
+-- instance Monad m => MonadPrec () m where
+--   askPrec   = return ()
+--   localPrec = const id
+
+-- | Precedence monad transformer.
+newtype PrecT p m a = PrecT { unPrecT :: ReaderT p m a }
+  deriving (Functor, Applicative, Monad, MonadTrans)
+
+-- | Running precedence-reading computation.
+runPrecT' :: PrecT p m a -> p -> m a
+runPrecT' = runReaderT . unPrecT
+
+-- | Running computation with default precedence.
+runPrecT :: TopPrecedence p => PrecT p m a -> m a
+runPrecT = (`runPrecT'` topPrecedence)
+
 -- | Abstract interface for monadic printing.
 
-class (Monad m, TopPrecedence p) => MonadPretty p m a | a -> p where
-  prettyM     :: a -> m Doc
-  prettyPrecM :: p -> a -> m Doc
+class MonadPrec p m => PrettyM p m a where
+  prettyPrecM :: a -> m Doc
 
-  prettyM     = prettyPrecM topPrecedence
-  prettyPrecM = const prettyM
+instance (Applicative m, MonadPrec p m) => PrettyM p m Doc where
+  prettyPrecM = pure
 
--- instance (Monad m, TopPrecedence p) => MonadPretty p m Doc where
---   prettyM = return
+-- | Print something in delimiters if some condition holds.
+bracketIf :: (TopPrecedence p, Functor m, Monad m, MonadPrec p m)
+  => Doc          -- ^ Opening bracket.
+  -> Doc          -- ^ Closing bracket.
+  -> (p -> Bool)  -- ^ Condition on precedence whether to bracket.
+  -> m Doc        -- ^ Inner computation, run in top context if bracketing.
+  -> m Doc        -- ^ Result is bracketed if condition held.
+bracketIf open close test cont =
+  -- If test fails on precedence, just continue.
+  ifNotM (test <$> askPrec) cont $ {- else -}
+  -- Other wise, continue in top context, and wrap result in delimiters.
+  inTopPrec cont <&> \ d -> open <> d <> close
+
+-- | Print something in parentheses if some condition holds.
+parensIf ::  (TopPrecedence p, Functor m, Monad m, MonadPrec p m)
+  => (p -> Bool)  -- ^ Condition on precedence whether to parenthesize.
+  -> m Doc        -- ^ Inner computation, run in top context if parenthesizing.
+  -> m Doc        -- ^ Result is parenthesized if condition held.
+parensIf = bracketIf lparen rparen
+
+-- Needs {-# LANGUAGE AllowAmbiguousTypes #-}
+-- prettyM :: (TopPrecedence p, PrettyM p (PrecT p m) a) => a -> m Doc
+-- prettyM = runPrecT . prettyPrecM
 
 -- * 'Doc' utilities
 
